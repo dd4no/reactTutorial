3faa602224a89d38bd878715565b87d684619176
Dave Defourneaux <dave.defourneaux@gmail.com>
2022-05-16 16:10:36 -0400

    simple list

--- src/index.js
+++ src/index.js
@@ -1,38 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "./index.css";
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51x8pmqjY0L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
-  title: "Oh, the Places You'll Go!",
-  author: "Dr. Seuss",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/41rzRPDRxJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
-  title: "Where the Crawdads Sing",
-  author: "Delia Owens",
-};
-
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51x8pmqjY0L._SX218_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Oh, the Places You'll Go!",
+    author: "Dr. Seuss",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/41rzRPDRxJL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+    title: "Where the Crawdads Sing",
+    author: "Delia Owens",
+  },
+];
+const names = ["dave", "joe", "jim"];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+console.log(newNames);
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
-          reiciendis nulla molestias dignissimos possimus consequuntur
-          exercitationem. Similique velit tenetur non!
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 
 const Book = (props) => {
@@ -43,7 +30,6 @@ const Book = (props) => {
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
